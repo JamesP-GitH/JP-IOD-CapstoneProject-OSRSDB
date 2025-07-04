28c3d2a282afccffe0ac2c101d999323d7d61a43
JamesP-GitH <james.pagliaccio@gmail.com>
2025-07-04 11:31:22 +1000

    added second route test

--- my-capstone-app/src/app/api/weapons/[id]/route.js
+++ my-capstone-app/src/app/api/weapons/[id]/route.js
@@ -1,8 +1,21 @@
 import dbConnect from "@/lib/dbConnect";
-import { weaponController } from "@/controllers";  
+import { weaponController } from "@/controllers";
 
-await async function GET(_, { params }) {
+export async function GET(_, { params }) {
     await dbConnect();
     const result = await weaponController.getWeaponById(params.id);
-    return Response.json(result)
+    return Response.json(result);
+}
+
+export async function PUT(req, { params }) {
+    await dbConnect();
+    const data = await req.json();
+    const result = await weaponController.updateWeapon(params.id, data);
+    return Response.json(result);
+}
+
+export async function DELETE(_, { params }) {
+    await dbConnect();
+    const result = await weaponController.deleteWeapon(params.id);
+    return Response.json(result);
 }
