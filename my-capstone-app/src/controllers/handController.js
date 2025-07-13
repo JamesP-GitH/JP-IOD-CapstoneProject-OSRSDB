90a02041809a6f07a419893c45c6e08015b827a3
JamesP-GitH <james.pagliaccio@gmail.com>
2025-07-14 09:30:02 +1000

    refactor: convert hand controller to use ES6 modules and async/await syntax

--- my-capstone-app/src/controllers/handController.js
+++ my-capstone-app/src/controllers/handController.js
@@ -1,96 +1,48 @@
-"use strict";
-let Models = require("../models");
+import { Hand } from "@/models";
 
-// Get all hand items
-const getAllHands = (req, res) => {
-    Models.Hand.find({})
-        .then((data) => res.send({ result: 200, data }))
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function getAllHands() {
+    return await Hand.find({});
+}
 
-// Get a single hand item by its ID
-const getHandById = (req, res) => {
-    Models.Hand.findById(req.params.id)
-        .then((data) => {
-            if (!data) {
-                return res.status(404).send({ result: 404, message: "Hand not found" });
-            }
-            res.send({ result: 200, data });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
-// Get hand items by name
-const getHandByName = (req, res) => {
-    Models.Hand.find({ name: { $regex: req.params.name, $options: "i" } })
-        .then((data) => {
-            if (data.length === 0) {
-                return res.status(404).json({ message: "No hands found matching that name" });
-            }
-            res.send({ result: 200, data });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function getHandById(id) {
+    const hand = await Hand.findById(id);
+    if (!hand) {
+        throw new Error("Hand not found");
+    }
+    return hand;
+}
 
-// Get all hand names only
-const getHandNames = (req, res) => {
-    Models.Hand.find({}, { name: 1, _id: 0 })
-        .then((data) => {
-            const names = data.map((item) => item.name);
-            res.send({ result: 200, data: names });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function getHandByName(name) {
+    const hands = await Hand.find({ name: { $regex: name, $options: "i" } });
+    if (hands.length === 0) {
+        throw new Error("No hand items found matching that name");
+    }
+    return hands;
+}
 
-// Create a new hand item
-const createHand = (req, res) => {
-    const newHand = new Models.Hand(req.body);
-    newHand
-        .save()
-        .then((data) => res.send({ result: 200, data }))
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function getHandNames() {
+    const data = await Hand.find({}, { name: 1, _id: 0 });
+    const names = data.map((item) => item.name);
+    return names;
+}
 
-// Update an existing hand item by ID
-const updateHand = (req, res) => {
-    Models.Hand.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then((data) => res.send({ result: 200, data }))
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function createHand(handData) {
+    const newHand = new Hand(handData);
+    return await newHand.save();
+}
 
-// Delete a hand item by ID
-const deleteHand = (req, res) => {
-    Models.Hand.findByIdAndDelete(req.params.id)
-        .then((data) => res.send({ result: 200, data }))
-        .catch((err) => {
-            console.log(err);
-            res.send({ result: 500, error: err.message });
-        });
-};
+export async function updateHand(id, handData) {
+    const updatedHand = await Hand.findByIdAndUpdate(id, handData, { new: true });
+    if (!updatedHand) {
+        throw new Error("Hand not found for update");
+    }
+    return updatedHand;
+}
 
-module.exports = {
-    getAllHands,
-    getHandById,
-    getHandByName,
-    getHandNames,
-    createHand,
-    updateHand,
-    deleteHand,
-};
+export async function deleteHand(id) {
+    const deletedHand = await Hand.findByIdAndDelete(id);
+    if (!deletedHand) {
+        throw new Error("Hand not found for deletion");
+    }
+    return deletedHand;
+}
