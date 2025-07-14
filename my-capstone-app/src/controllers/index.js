cce3c0bbb7b17b23ce008097ccecc8577460fd32
JamesP-GitH <james.pagliaccio@gmail.com>
2025-07-14 11:17:28 +1000

    refactor: streamline controller exports and added generic item id search function for dynamic page

--- my-capstone-app/src/controllers/index.js
+++ my-capstone-app/src/controllers/index.js
@@ -1,13 +1,39 @@
+const weaponController = require("./weaponController");
+const shieldController = require("./shieldController");
+const headController = require("./headController");
+const handController = require("./handController");
+const bodyController = require("./bodyController");
+const capeController = require("./capeController");
+const ammoController = require("./ammoController");
+const feetController = require("./feetController");
+const legController = require("./legController");
+const neckController = require("./neckController");
+const ringController = require("./ringController");
+
+const { Weapon, Shield, Head, Hand, Body, Cape, Ammo, Feet, Leg, Neck, Ring } = require("@/models");
+
+const itemModels = [Weapon, Shield, Head, Hand, Body, Cape, Ammo, Feet, Leg, Neck, Ring];
+
+// Generic search across all gear slot models
+async function getItemById(id) {
+    for (const model of itemModels) {
+        const item = await model.findById(id);
+        if (item) return item;
+    }
+    return null;
+}
+
 module.exports = {
-    weaponController: require("./weaponController"),
-    shieldController: require("./shieldController"),
-    headController: require("./headController"),
-    handController: require("./handController"),
-    bodyController: require("./bodyController"),
-    capeController: require("./capeController"),
-    ammoController: require("./ammoController"),
-    feetController: require("./feetController"),
-    legController: require("./legController"),
-    neckController: require("./neckController"),
-    ringController: require("./ringController"),
+    weaponController,
+    shieldController,
+    headController,
+    handController,
+    bodyController,
+    capeController,
+    ammoController,
+    feetController,
+    legController,
+    neckController,
+    ringController,
+    getItemById,
 };
